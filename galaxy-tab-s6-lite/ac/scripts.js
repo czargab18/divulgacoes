a4eb6eca043b0871934561949bf64ca39a7b7383
czargab18 <cesargabrielcode@gmail.com>
2023-12-27 18:33:09 -0300

    uploand: alteração de preços e links dinamicamente

--- galaxy-tab-s6-lite/ac/scripts.js
+++ galaxy-tab-s6-lite/ac/scripts.js
@@ -1,9 +1,36 @@
+function changeProduct(imageSrc, circleColor, newPrice, newLink) {
+  // Altera a imagem e a cor do círculo conforme necessário
+  imgSlider(imageSrc);
+  circleChange(circleColor);
+
+  // Altera dinamicamente o preço e o link da tag <a>
+  document.getElementById("price").innerText = newPrice;
+  document.getElementById("amazon-link").href = newLink;
+}
+
 /*Função de mudar o celular*/
 function imgSlider(e) {
   document.querySelector("#phone").src = e;
 }
+
 /*Função de mudar a cor do circulo*/
 function circleChange(color) {
   const circle = document.querySelector(".circle");
   circle.style.background = color;
 }
+
+function changeText(newText) {
+  document.getElementById("amazon-link").innerText = newText;
+}
+
+document.getElementById("green").addEventListener("click", function () {
+  changeText("R$ 1.575");
+});
+
+document.getElementById("pink").addEventListener("click", function () {
+  changeText("R$ 1.999");
+});
+
+document.getElementById("blue").addEventListener("click", function () {
+  changeText("R$ 1.549");
+});
